6c35a83286882c31ce9c0ff3900f30b77988e958
babi2707 <90732683+babi2707@users.noreply.github.com>
2025-04-24 10:12:35 +0100

    feat: Implement date and task input fields with date picker
    This commit introduces the initial implementation for adding tasks to the ToDo list.
    - Added new files for Android project system configurations.
    - Implemented `ToDoLayout` composable to structure the main UI.
    - Added `EditDateField` composable to allow users to input date, with a date picker dialog.
    - Added `EditTaskField` composable to allow users to input tasks.
    - Added string resources for date, task and button labels.
    - Implemented a button to add the inputs, yet without any functionality.
    - Added necessary imports and dependencies.

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -5,14 +5,48 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todolist.ui.theme.ToDoListTheme
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,11 +54,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ToDoListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    ToDoLayout()
                 }
             }
         }
@@ -32,10 +65,141 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ToDoLayout() {
+    var dateInput by remember { mutableStateOf("") }
+    var taskInput by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .statusBarsPadding()
+            .padding(horizontal = 40.dp)
+            .verticalScroll(rememberScrollState())
+            .safeDrawingPadding(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            text = stringResource(R.string.add_date),
+            modifier = Modifier
+                .padding(bottom = 16.dp, top = 40.dp)
+                .align(alignment = Alignment.Start)
+        )
+
+        EditDateField(
+            value = dateInput,
+            onValueChange = { dateInput = it },
+            modifier = Modifier
+                .padding(bottom = 5.dp)
+                .fillMaxWidth()
+                .background(color = Color.Magenta)
+        )
+
+        Text(
+            text = stringResource(R.string.add_task),
+            modifier = Modifier
+                .padding(bottom = 16.dp, top = 10.dp)
+                .align(alignment = Alignment.Start)
+        )
+
+        EditTaskField(
+            value = taskInput,
+            onValueChange = { taskInput = it },
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .fillMaxWidth()
+                .background(color = Color.Magenta)
+        )
+
+        Spacer(modifier = Modifier.height(5.dp))
+
+        Button(onClick = { }) {
+            Text(stringResource(R.string.add_button))
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditDateField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier){
+
+    val showDatePicker = remember { mutableStateOf(false) }
+
+    val dateFormatter = remember {
+        SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+    }
+
+    TextField(
+        value = value,
+        onValueChange = {},
+        label = { Text(stringResource(R.string.add_date_label)) },
         modifier = modifier
+            .fillMaxWidth()
+            .clickable { showDatePicker.value = true },
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
+        readOnly = true,
+        trailingIcon = {
+            Icon(
+                imageVector = Icons.Default.DateRange,
+                contentDescription = "Selecionar data"
+            )
+        }
+    )
+
+    if (showDatePicker.value) {
+        val calendar = Calendar.getInstance()
+        val datePickerState = rememberDatePickerState(
+            initialSelectedDateMillis = try {
+                dateFormatter.parse(value)?.time ?: calendar.timeInMillis
+            } catch (e: Exception) {
+                calendar.timeInMillis
+            }
+        )
+
+        DatePickerDialog(
+            onDismissRequest = { showDatePicker.value = false },
+            confirmButton = {
+                Button(
+                    onClick = {
+                        datePickerState.selectedDateMillis?.let { millis ->
+                            val dateStr = dateFormatter.format(Date(millis))
+                            onValueChange(dateStr)
+                        }
+                        showDatePicker.value = false
+                    }
+                ) {
+                    Text("OK")
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick = { showDatePicker.value = false }
+                ) {
+                    Text("Cancelar")
+                }
+            }
+        ) {
+            DatePicker(state = datePickerState)
+        }
+    }
+}
+
+@Composable
+fun EditTaskField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier){
+
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = { Text(stringResource(R.string.add_task_label)) },
+        modifier = modifier.fillMaxWidth(),
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
     )
 }
 
@@ -43,6 +207,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     ToDoListTheme {
-        Greeting("Android")
+        ToDoLayout()
     }
 }
